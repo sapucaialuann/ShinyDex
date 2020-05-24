b1cb44b7f47f67fb588e2258e459dd5aea4029d3
Luann Sapucaia <luannsapucaia@gmail.com>
2020-05-24 03:15:48 -0300

    Card created and pokemon number added to the card

--- src/components/PokemonImage/index.js
+++ src/components/PokemonImage/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
+import './pokemonImage.css';
 
 function PokeImage() {
     const [data, setData] = useState({ results: []});
@@ -7,13 +8,14 @@ function PokeImage() {
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await api.get('/pokemon?offset=0&limit=150');
+            const response = await api.get('/pokemon?offset=0&limit=27');
             const details = await Promise.all(response.data.results.map((res) => {
                 return api.get(`/pokemon/${res.name}`)
             }));
             const results = details.map(res => ({
                 image: res.data.sprites,
-                name: res.data.name
+                name: res.data.name,
+                id: res.data.id
             }));
             setData({results});
 
@@ -23,13 +25,22 @@ function PokeImage() {
 
 
     return(
-        <ul>
-            {data.results.map(item => (
-            <li><img src={item.image.front_shiny} />
-            <img src={item.image.back_shiny} />
-            <h2>Shiny {item.name}</h2></li>
-            ))}
-        </ul>
+        <div className="cardsContainer">
+            <ul className="pokemonBoard" >
+                {data.results.map(item => (
+                <li>
+                    <div className="pokemonImages">
+                        <img src={item.image.front_shiny} alt={item.name}/>
+                        {/* <img src={item.image.back_shiny} /> */}
+                    </div>
+                    <div className="pokemonData">
+                        <h2 className="pokedexNumber" >#{item.id}</h2>
+                        <h2>{item.name}</h2>
+                    </div>
+                </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
